3073362d4791d4c3845b72a9eea93b45517e5517
ANH VA NHO <anhvanho@gmail.com>
2026-07-13 10:05:17 +0700

    Increase express body parser limit to support base64 images

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const { exec } = require('child_process');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Session storage in memory
 // token -> { lastSeen: timestamp }
